126d5ebb08817c0cedd7ae28f810d4b3602e18ce
Dan Cohn <dacohn@gmail.com>
2014-01-02 21:37:12 -0500

    Added logic to correctly increment weekly due dates

--- RemindMe/DCRecurringInfo.m
+++ RemindMe/DCRecurringInfo.m
@@ -51,8 +51,65 @@
             break;
             
         case DCRecurringInfoRepeatsWeekly:
-            interval = 60 * 60 * 24 * 7 * self.repeatIncrement;
-            return [startDate dateByAddingTimeInterval:interval];
+            // No days selected to repeat on, so just increment one week from startDate
+            if ( self.daysToRepeat == nil || self.daysToRepeat.count == 0 )
+            {
+                interval = 60 * 60 * 24 * 7 * self.repeatIncrement;
+                return [startDate dateByAddingTimeInterval:interval];
+            }
+            // Only schedule task on specified days
+            else
+            {
+                NSInteger daysToNextDueDate = 8; // Repeats weekly so start with a value greater than 7
+                NSDateComponents *incrementDateComponent = [[NSDateComponents alloc] init];
+                NSDateComponents *lastDueDateComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitWeekday | NSCalendarUnitWeekOfYear fromDate:lastDueDate];
+                NSUInteger weekdayToday = [lastDueDateComponents weekday];
+                NSDate *newDate = nil;
+                
+                // If set to repeat the same day each week, just add 1 week to the lastDueDate
+                if ( self.daysToRepeat.count == 1 )
+                {
+                    [incrementDateComponent setWeek:1];
+                    newDate = [[NSCalendar currentCalendar] dateByAddingComponents:incrementDateComponent toDate:lastDueDate options:0];
+                }
+                // If set to repeat multiple times per week, figure out the next day
+                else
+                {
+                    // Loop through each day to repeat, looking for the minimum number of days between now and the next day
+                    for ( NSNumber *day in self.daysToRepeat )
+                    {
+                        NSInteger daysToNextDay = (7 + 1 + [day intValue] - weekdayToday) % 7;
+                        // Ignore results of 0 because that's just the same day as the lastDueDate
+                        if ( daysToNextDay != 0 && daysToNextDay < daysToNextDueDate )
+                        {
+                            daysToNextDueDate = daysToNextDay;
+                        }
+                    }
+                    
+                    // Increment by number of days to next repeat day
+                    [incrementDateComponent setDay:daysToNextDueDate];
+                    newDate = [[NSCalendar currentCalendar] dateByAddingComponents:incrementDateComponent toDate:lastDueDate options:0];
+                }
+                
+                // Skip weeks if repeatIncrement is greater than 1
+                if ( self.repeatIncrement > 1 )
+                {
+                    // If current date is in the same week as the last due date, no need to skip weeks
+                    NSUInteger lastDueDateWeek = [lastDueDateComponents weekOfYear];
+                    NSDateComponents *newDateComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitWeekOfYear fromDate:newDate];
+                    NSUInteger nextDueDateWeek = [newDateComponents weekOfYear];
+                    
+                    // Weeks are different so skip ahead how every many weeks are necessary
+                    if ( lastDueDateWeek != nextDueDateWeek )
+                    {
+                        NSDateComponents *addWeeksComponent = [[NSDateComponents alloc] init];
+                        // Subtract one week because we already skipped to the next one
+                        [addWeeksComponent setWeek:self.repeatIncrement - 1];
+                        newDate = [[NSCalendar currentCalendar] dateByAddingComponents:addWeeksComponent toDate:newDate options:0];
+                    }
+                }
+                return newDate;
+            }
             break;
             
         case DCRecurringInfoRepeatsMonthly:
